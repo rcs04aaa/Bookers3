2384b03c2e6686af34b6fad0f2db40aa9ffa9f10
rcs04aaa <122098552+rcs04aaa@users.noreply.github.com>
2023-02-05 12:39:44 +0000

    [finish]Bookers3

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,13 +3,13 @@ class FavoritesController < ApplicationController
   def create
     @post_favorite = Favorite.new(user_id: current_user.id, book_id: params[:book_id])
     @post_favorite.save
-    # redirect_to book_path(params[:book_id]) 
+    redirect_to book_path(params[:book_id]) 
   end
   
   def destroy
     @post_favorite = Favorite.find_by(user_id: current_user.id, book_id: params[:book_id])
     @post_favorite.destroy
-    # redirect_to book_path(params[:book_id])
+    redirect_to book_path(params[:book_id])
   end
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ApplicationRecord
   has_one_attached :profile_image
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
   
   def get_profile_image
     unless profile_image.attached?
